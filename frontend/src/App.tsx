69975e615a6895b3ef3c4e1384af25e2fc4d7d30
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-06-23 15:15:44 +0200

    Fetch todo items from server

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,30 @@
 import {Todo} from "./Todo.ts";
 import TodoCard from "./TodoCard.tsx";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function App() {
 
-    const todos: Todo[] = [
-        {
-            "id": "abc123",
-            "description": "kochen",
-            "status": "OPEN"
-        },
-        {
-            "id": "abc124",
-            "description": "putzen",
-            "status": "OPEN"
-        }
-    ]
+    const [todos, setTodos] = useState<Todo[]>()
+
+    useEffect(
+        () => {
+            axios.get("/api/todo")
+                .then(response => {
+                    setTodos(response.data)
+                })
+        }, []
+    )
+
+    if (!todos) {
+        return "Lade..."
+    }
 
     return (
         <>
             <h1>TODOs</h1>
             {
-                todos.map(todo => <TodoCard todo={todo} key={todo.id} />)
+                todos.map(todo => <TodoCard todo={todo} key={todo.id}/>)
             }
         </>
     )
