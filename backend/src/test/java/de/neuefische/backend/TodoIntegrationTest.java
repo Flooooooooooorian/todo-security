28d7bf791d92148f5b55658ca45e985989cfad22
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2023-10-19 10:55:55 +0200

    Fix Tests

--- backend/src/test/java/de/neuefische/backend/TodoIntegrationTest.java
+++ backend/src/test/java/de/neuefische/backend/TodoIntegrationTest.java
@@ -6,9 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
@@ -27,10 +25,6 @@ public class TodoIntegrationTest {
 
     @Autowired
     ObjectMapper objectMapper;
-
-    @MockBean
-    ClientRegistrationRepository ClientRegistrationRepository;
-
     @Test
     void expectEmptyListOnGet() throws Exception {
         mockMvc.perform(get("http://localhost:8080/api/todo"))
